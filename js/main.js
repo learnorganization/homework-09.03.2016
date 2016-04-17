59f12380d69714cd503d735259972c8fd84031f5
Dmitriy Geytenko <m0rientes@rambler.ru>
2016-04-17 21:29:21 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -42,21 +42,9 @@ $('.responsive').slick({
 });
   
   jQuery(document).ready(function($){
-
-   
-
-      /* Подготавливаем иконку меню */
-
       $('#nav-wrap').prepend('<div id="menu-icon">Меню</div>');
-
-       
-
-      /* Включаем навигацию */
-
       $("#menu-icon").on("click", function(){
-
           $("#nav").slideToggle();
-
           $(this).toggleClass("active");
       });
   });
